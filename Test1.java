c40ad21acce92e923461a950d059ba6847c140ff
aritrac <aritrachatterjee2007@gmail.com>
2017-05-17 19:25:03 +0530

    Commit 2

---
+++ Test1.java
@@ -0,0 +1,26 @@
+/**
+ * SPOJ CODE -> TESTINT
+ * Given two natural numbers (both not greater than 200), each number in the separate line, please print the sum of them.
+ * 
+ * Example
+
+   Input:
+   2
+   3
+
+   Output:
+   5
+ */
+import java.util.*;
+import java.lang.*;
+ 
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+	}
+} 
